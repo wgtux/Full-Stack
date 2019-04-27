031c12f574aea89290d06a9ed8bfc57fa72a95f8
wgtux <weslengomes@gmail.com>
2019-04-27 12:47:33 -0300

    Criando o jogo mata mosquito no navegador

---
+++ JavaScript/Mata Mosquito/game.js
@@ -0,0 +1,31 @@
+var altura = 0
+var largura = 0
+
+function tamanhoPalco(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+tamanhoPalco()
+
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criando o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
