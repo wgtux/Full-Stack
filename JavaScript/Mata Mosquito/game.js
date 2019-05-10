1010f8634cb7f79647f2b71563456d0b1816f0de
wgtux <weslengomes@gmail.com>
2019-05-10 17:49:40 -0300

    fundo e movimentaçao automatica do mosquito

--- JavaScript/Mata Mosquito/game.js
+++ JavaScript/Mata Mosquito/game.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 
+//definindo o tamanho do palco do jogo
 function tamanhoPalco(){
     altura = window.innerHeight
     largura = window.innerWidth
@@ -10,7 +11,14 @@ function tamanhoPalco(){
 
 tamanhoPalco()
 
+
 function posicaoRandomica(){
+
+    //remover o mosquito anterior (caso ele exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -22,10 +30,43 @@ function posicaoRandomica(){
     //criando o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
-    mosquito.style.position = 'absolute'
+    mosquito.style.position = 'absolute' 
+    mosquito.id = 'mosquito'
 
     document.body.appendChild(mosquito)
+
+    
 }
+
+// Função para criação do tamanho aleatorio do mosquito
+function tamanhoAleatorio(){
+    var tamanhoMosquito = Math.floor(Math.random()*3)
+
+    switch(tamanhoMosquito){
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+           return 'mosquito3'
+    }
+}
+
+//Mudando o lado que o mosquito olha de forma randomica
+function ladoAleatorio(){
+    var lado = Math.floor(Math.random()*2)
+
+    switch(lado){
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+    }
+}
+
