99788860d7d346e540733146065be5405a497d9d
Weslen <weslengomes@gmail.com>
2020-07-02 19:52:32 -0300

    inicio do CRUD

---
+++ PHP/aulas/bdteste/config.php
@@ -0,0 +1,7 @@
+<?php
+$db_name = 'teste';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '?W3sl3n2601!';
+
+$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
---
+++ PHP/aulas/bdteste/index.php
@@ -0,0 +1,17 @@
+<?php
+require 'config.php';
+
+?>
+
+<br>
+<a href="adicionar.php">Adicionar Novo Usuario</a>
+<br>
+<br>
+<table border="1" width="100%">
+    <tr>
+        <th> ID</th>
+        <th> NOME</th>
+        <th> EMAIL</th>
+        <th> AÇÕES</th>
+    </tr>
+</table>
