ff3a5215332cd91c0a1e02f27df6052d0ddb7f35
Weslen <weslengomes@gmail.com>
2020-07-04 18:05:03 -0300

    Adicionando verificação de email duplicado

--- PHP/aulas/bdteste/adicionar_action.php
+++ PHP/aulas/bdteste/adicionar_action.php
@@ -6,14 +6,25 @@ $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
 
 if($name && $email){
-    //Cria um template para segurança do banco de dados
-    $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES (:name, :email)");
-    $sql->bindValue(':name', $name);
+    //consulta o banco para verificar se tem email igual
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
     $sql->bindValue(':email', $email);
     $sql->execute();
 
-    header("Location: index.php");
-    exit;
+    if($sql->rowCount() === 0){
+        //Cria um template para segurança do banco de dados
+        $sql = $pdo->prepare("INSERT INTO usuarios (name, email) VALUES (:name, :email)");
+        $sql->bindValue(':name', $name);
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+
+        header("Location: index.php");
+        exit;
+    }
+    else{
+        header("Location: adicionar.php");
+        exit;
+    }
 }
 else{
     header("Location: adicionar.php");
--- PHP/aulas/bdteste/config.php
+++ PHP/aulas/bdteste/config.php
@@ -2,6 +2,6 @@
 $db_name = 'teste';
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = '?W3sl3n2601!';
+$db_pass = '';
 
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
